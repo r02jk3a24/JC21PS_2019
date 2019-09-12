9c2d331bb318710ced848dd5e54cc1b93460559b
170039 <170039@e0505-40.JC-21.local>
2019-09-12 14:50:21 +0900

    未完成

--- JC21PS/src/jp/co/jcps/A05/JoinRequestControllerServlet.java
+++ JC21PS/src/jp/co/jcps/A05/JoinRequestControllerServlet.java
@@ -44,7 +44,8 @@ public class JoinRequestControllerServlet extends HttpServlet {
 		 *  ヒント
 		 *  セッションには「userId」という名前でログインユーザーIDが格納されている。
 		 */
-		String userId = ;
+		String userId =(String) request.getSession().getAttribute("userId") ;
+
 
 		// SQLを宣言
 		String sql = "SELECT * FROM mst_club WHERE club_id NOT IN (SELECT club_id FROM trn_join_request WHERE user_id = ?) AND club_id NOT IN (SELECT club_id FROM trn_club_member WHERE user_id = ?);";
@@ -58,7 +59,8 @@ public class JoinRequestControllerServlet extends HttpServlet {
 		 *  ログインユーザーの情報を使う。
 		 */
 
-
+		paramList.add((String) request.getSession().getAttribute("user_Id"));
+		paramList.add((String) request.getParameter("clubId"));
 
 
 		// DB接続を初期化
--- JC21PS/src/jp/co/jcps/A05/JoinRequestSaveServlet.java
+++ JC21PS/src/jp/co/jcps/A05/JoinRequestSaveServlet.java
@@ -43,7 +43,7 @@ public class JoinRequestSaveServlet extends HttpServlet {
 
 
 		// TODO: データベースにデータを登録する為のSQL文を完成させなさい。
-		String sql = "INSERT INTO";
+		String sql = "INSERT INTO trm_join_request(user_id,club_id) values(?,?) ";
 
 		// SQLに埋め込むパラメータリストを定義
 		List<String> paramList = new ArrayList<String>();
@@ -56,7 +56,11 @@ public class JoinRequestSaveServlet extends HttpServlet {
 		 * リクエストパラメータの取得のrequest.getParameter(【HTMLのname属性の値】)で取得可能
 		 * A04,ParticipantListControllerServlet.java 43行目を参照
 		 */
-
+		
+		String userId = (String)request.getSession().getAttribute("userId");
+		String clubId = (String)request.getParameter("activityId");
+		
+		String sql ="INSERT INTO trm_join_reqest values("+userId+",'"+clubId+"')";
 
 
 		// SQLを実行しデータを登録
