e8f371b913768f9ba0f9627ce576957e76659ddb
170003 <170003@e0505-07.JC-21.local>
2019-09-12 14:40:19 +0900

    未完成のファイルを保存

--- JC21PS/src/jp/co/jcps/A05/JoinRequestControllerServlet.java
+++ JC21PS/src/jp/co/jcps/A05/JoinRequestControllerServlet.java
@@ -44,20 +44,20 @@ public class JoinRequestControllerServlet extends HttpServlet {
 		 *  ヒント
 		 *  セッションには「userId」という名前でログインユーザーIDが格納されている。
 		 */
-		String userId = ;
+		String userId = (String) request.getSession().getAttribute("userId");
 
 		// SQLを宣言
 		String sql = "SELECT * FROM mst_club WHERE club_id NOT IN (SELECT club_id FROM trn_join_request WHERE user_id = ?) AND club_id NOT IN (SELECT club_id FROM trn_club_member WHERE user_id = ?);";
 
 		// SQLに埋め込むパラメータリストを定義
-		List<String> paramList = new ArrayList<String>();
+	      List<String> paramList = new ArrayList<String>();
 		/* TODO: SQLに埋め込む値をparamListに設定しなさい。
 		 *  ヒント①
 		 *  Listにはaddメソッドで要素を追加することができる。
 		 *  ヒント②
 		 *  ログインユーザーの情報を使う。
 		 */
-
+	      paramList.add(userId);
 
 
 
