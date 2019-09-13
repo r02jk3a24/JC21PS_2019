46691d5c52576da679ecb5c77502537f08d54f76
170435 <170435@e0505-16.JC-21.local>
2019-09-13 11:05:59 +0900

    完成したファイル

--- JC21PS/src/jp/co/jcps/A05/JoinRequestSaveServlet.java
+++ JC21PS/src/jp/co/jcps/A05/JoinRequestSaveServlet.java
@@ -56,10 +56,9 @@ public class JoinRequestSaveServlet extends HttpServlet {
 		 * リクエストパラメータの取得のrequest.getParameter(【HTMLのname属性の値】)で取得可能
 		 * A04,ParticipantListControllerServlet.java 43行目を参照
 		 */
-		//paramList.add((String) request.getSession().getAttribute("userId"));
-		//paramList.add((String) request.getParameter("clubId"));
-		String userId = (String) request.getParameter("userId");
-		String clubId = (String) request.getParameter("clubId");
+		paramList.add((String) request.getSession().getAttribute("userId"));
+		paramList.add((String) request.getParameter("registClubId"));
+
 		// SQLを実行しデータを登録
 		DBConnection db = new DBConnection();
 		try {
