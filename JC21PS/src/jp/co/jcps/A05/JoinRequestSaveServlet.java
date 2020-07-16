2d1b47d4e08abb8ec592952b26fede50e45644a7
180005 <180005@e0505-42.JC-21.local>
2020-07-16 15:54:29 +0900

    07/16現在

--- JC21PS/src/jp/co/jcps/A05/JoinRequestSaveServlet.java
+++ JC21PS/src/jp/co/jcps/A05/JoinRequestSaveServlet.java
@@ -43,7 +43,7 @@ public class JoinRequestSaveServlet extends HttpServlet {
 
 
 		// TODO: データベースにデータを登録する為のSQL文を完成させなさい。
-		String sql = "INSERT INTO";
+		String sql = "INSERT INTO mst_club(club_id, club_name, club_description) VALUES (?,?,?);";
 
 		// SQLに埋め込むパラメータリストを定義
 		List<String> paramList = new ArrayList<String>();
@@ -56,8 +56,8 @@ public class JoinRequestSaveServlet extends HttpServlet {
 		 * リクエストパラメータの取得のrequest.getParameter(【HTMLのname属性の値】)で取得可能
 		 * A04,ParticipantListControllerServlet.java 43行目を参照
 		 */
-
-
+		paramList.add((String) request.getSession().getAttribute("user_id"));
+		paramList.add(request.getParameter("club_id"));
 
 		// SQLを実行しデータを登録
 		DBConnection db = new DBConnection();
--- JC21PS/src/jp/co/jcps/A07/ClubInfoRegisterControllerServlet.java
+++ JC21PS/src/jp/co/jcps/A07/ClubInfoRegisterControllerServlet.java
@@ -1,6 +1,7 @@
 package jp.co.jcps.A07;
 
 import java.io.IOException;
+import java.sql.ResultSet;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -41,14 +42,14 @@ public class ClubInfoRegisterControllerServlet extends HttpServlet {
 		// セッションからログイン中のユーザーの部長クラブIDを取得する
 		// TODO: セッション（ログインユーザーの情報を保持している）からログインユーザーの部長を務める部活の部活IDを取得する。
 
-
+		String leaderClubId = (String) request.getSession().getAttribute("leaderClubId");
 		// TODO:SQLを宣言
 
-
+		String sql = "SELECT club_name, club_description FROM mst_club WHERE club_id = ? ";
 		// TODO: SQLに埋め込むパラメータリストを定義
 		List<String> paramList = new ArrayList<String>();
 
-
+		paramList.add(leaderClubId);
 		// DB接続を初期化
 		DBConnection db = new DBConnection();
 
@@ -57,11 +58,11 @@ public class ClubInfoRegisterControllerServlet extends HttpServlet {
 
 		try {
 			// TODO; DBから情報を取得
-
+			ResultSet rs = db.executeSelectQuery(sql, paramList);
 			while (rs.next()) {
 				// TODO: beanにDBから取得した値をセット
-
-
+				bean.setClubName(rs.getString("club_name"));
+				bean.setClubDescription(rs.getString("club_description"));
 			}
 		} catch (Exception e) {
 			System.out.println(e.getMessage());
