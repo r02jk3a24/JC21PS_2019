3a3b99898ac26f31977f8e75df8656cbc2b91ea3
yoshzawa <yoshzawa@jc-21.jp>
2020-07-16 16:33:08 +0900

    change

--- JC21PS/src/jp/co/jcps/A05/JoinRequestControllerServlet.java
+++ JC21PS/src/jp/co/jcps/A05/JoinRequestControllerServlet.java
@@ -59,6 +59,7 @@ public class JoinRequestControllerServlet extends HttpServlet {
 		 */
 		paramList.add("userId");
 		paramList.add("userId");
+		
 
 		// DB接続を初期化
 		DBConnection db = new DBConnection();
