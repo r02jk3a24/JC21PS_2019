c52adfd5846660abea865f6e92f7d2f7f9c16e12
180045 <180045@e0505-25.JC-21.local>
2020-07-16 15:45:33 +0900

    07/16現在

--- JC21PS/src/jp/co/jcps/A04/ParticipantListControllerServlet.java
+++ JC21PS/src/jp/co/jcps/A04/ParticipantListControllerServlet.java
@@ -41,7 +41,7 @@ public class ParticipantListControllerServlet extends HttpServlet {
 
 		// リクエストパラメーターから活動IDを取得する
 		// TODO: リクエストから遷移元でクリックされた活動IDを取得できるように44行目を変更しなさい。
-		String activityId = "";
+		String activityId = "activity_id";
 
 		// TODO: データベースから必要な情報を取得するためのSQL文を完成させなさい。
 		String sql = "SELECT ";
@@ -49,7 +49,7 @@ public class ParticipantListControllerServlet extends HttpServlet {
 		// SQLに埋め込むパラメータリストを定義
 		List<String> paramList = new ArrayList<String>();
 		// TODO: SQLに埋め込む値を設定しなさい。
-		
+
 
 		// DB接続を初期化
 		DBConnection db = new DBConnection();
@@ -72,7 +72,7 @@ public class ParticipantListControllerServlet extends HttpServlet {
 				DBから取得した情報はResultSetクラスのgetString()メソッドで取得する。
 				getStringメソッドの引数は取得したいカラム名を文字列で指定する。
 				 */
-
+				途中request.getString().次から
 
 			}
 		} catch (Exception e) {
