f6aa45df1429ee0d3283c5619d2854a973f385db
180562 <180562@e0505-10.JC-21.local>
2020-07-16 15:44:39 +0900

    07/16現在

--- JC21PS/src/jp/co/jcps/A07/ClubInfoRegisterControllerServlet.java
+++ JC21PS/src/jp/co/jcps/A07/ClubInfoRegisterControllerServlet.java
@@ -1,6 +1,7 @@
 package jp.co.jcps.A07;
 
 import java.io.IOException;
+import java.sql.ResultSet;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -41,27 +42,27 @@ public class ClubInfoRegisterControllerServlet extends HttpServlet {
 		// セッションからログイン中のユーザーの部長クラブIDを取得する
 		// TODO: セッション（ログインユーザーの情報を保持している）からログインユーザーの部長を務める部活の部活IDを取得する。
 
-
+		String leaderClubId = (String) request.getSession().getAttribute("leaderClubId");
 		// TODO:SQLを宣言
-
+		String sql = "SELECT club_name, club_description FROM mst_club WHERE club_id = ? ";
 
 		// TODO: SQLに埋め込むパラメータリストを定義
 		List<String> paramList = new ArrayList<String>();
 
+		paramList.add(leaderClubId);
 
 		// DB接続を初期化
 		DBConnection db = new DBConnection();
 
 		// 部活情報登録画面のBeanを初期化
 		ClubInfoRegisterBean bean = new ClubInfoRegisterBean();
-
 		try {
 			// TODO; DBから情報を取得
-
+			ResultSet rs = db.executeSelectQuery(sql, paramList);
 			while (rs.next()) {
 				// TODO: beanにDBから取得した値をセット
-
-
+				bean.setClubName(rs.getString("club_name"));
+				bean.setClubDescription(rs.getString("club_description"));
 			}
 		} catch (Exception e) {
 			System.out.println(e.getMessage());
